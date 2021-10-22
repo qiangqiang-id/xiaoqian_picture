9b83e4dd1f3cb4b55faec8c0d13bf7514afa0bd1
qiangqiang <qiangq24@outlook.com>
2021-10-22 16:42:38 +0800

    refactor: app

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,12 @@
-<script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from "./components/HelloWorld.vue";
-</script>
-
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
-  <el-button type="primary">Primary</el-button>
   <div>
     <p>你好呀</p>
   </div>
 </template>
+
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+</script>
 
 <style lang="scss"></style>
